c95af13584b5853871803e4e9134aa2beacb9078
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-03-16 19:29:16 +0000

    Fix api path

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 const apiApp = express();
 
-require("api/v1/prices.js")(apiApp);
+require("./api/v1/prices.js")(apiApp);
 
 apiApp.listen(PORT, () => console.log(`Listening on ${PORT}`));
